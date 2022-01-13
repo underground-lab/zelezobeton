b29f87c60aacfd257e3dd3353eac428ad322391f
Michal Porteš <michalportes1@gmail.com>
2022-01-13 09:12:12 +0100

    Retrieve version string from pyproject.toml

--- game/__init__.py
+++ game/__init__.py
@@ -1,5 +1,12 @@
+from pathlib import Path
+
+import toml
+
 from game.classes import Room, Object, Player, Game
 from game.data import room_data, object_data, player_data
+
+ROOT_DIR = Path(__file__).parent.parent
+__version__ = toml.load(ROOT_DIR / 'pyproject.toml')['tool']['poetry']['version']
 
 objects = {i: Object(**params) for i, params in object_data.items()}
 rooms = {i: Room(**params) for i, params in room_data.items()}
