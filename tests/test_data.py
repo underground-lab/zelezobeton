1b4c4e780ad4977011df37e29007baf4b704fef4
Michal Porteš <michalportes1@gmail.com>
2022-01-15 19:50:22 +0100

    Add tests for impact specs

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,6 +1,9 @@
 from collections import Counter
 from itertools import chain
 
+import pytest
+
+import game.callbacks
 from game.data import room_data, object_data, player_data
 
 
@@ -46,3 +49,36 @@ def test_no_object_occurs_more_than_once():
     )
     most_common, count = Counter(occurrences).most_common()[0]
     assert count <= 1, f'Object {most_common} occurs in more than one place'
+
+
+@pytest.fixture
+def impact_specs():
+    return [
+        impact_spec
+        for obj in object_data.values()
+        for action in obj.get('actions', {}).values()
+        for impact_spec in action['impact']
+    ]
+
+
+def test_impact_specs_use_existing_callback_names(impact_specs):
+    for impact_spec in impact_specs:
+        callback_name = impact_spec[0]
+        assert hasattr(game.callbacks, callback_name), \
+            f'Unknown callback {callback_name!r}'
+
+
+def test_impact_specs_use_existing_room_ids(impact_specs):
+    for impact_spec in impact_specs:
+        kwargs = impact_spec[1]
+        if 'room' in kwargs:
+            room_id = kwargs['room']
+            assert room_id in room_data, f'Unknown room {room_id} in {impact_spec}'
+
+
+def test_impact_specs_use_existing_object_ids(impact_specs):
+    for impact_spec in impact_specs:
+        kwargs = impact_spec[1]
+        if 'obj' in kwargs:
+            obj_id = kwargs['obj']
+            assert obj_id in object_data, f'Unknown object {obj_id} in {impact_spec}'
