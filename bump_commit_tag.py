5aa25e4fdd3d267c95566e31ba1edc1de344f6de
Michal Porteš <michalportes1@gmail.com>
2022-02-27 13:54:51 +0100

    Add script to bump version, add toml to dev dependencies

---
+++ bump_commit_tag.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import subprocess
+import sys
+
+import toml
+
+try:
+    rule = sys.argv[1]
+except IndexError:
+    rule = 'patch'
+
+# bump with poetry
+subprocess.run(f'poetry version {rule}', shell=True)
+
+# write new version to VERSION
+version = toml.load('pyproject.toml')['tool']['poetry']['version']
+with open('VERSION', 'w') as f:
+    print(version, file=f)
+
+# commit & tag
+subprocess.run(f'git commit -a -m "Bump version to {version}"', shell=True)
+subprocess.run(f'git tag {version}', shell=True)
