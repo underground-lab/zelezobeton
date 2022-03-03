2f9e73dc6d3abbf962c4f605b7bce46df02a47cf
Michal Porteš <michalportes1@gmail.com>
2022-03-03 10:48:32 +0100

    Move fixtures to conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+
+from engine import Game
+from game.data import room_data, object_data
+
+
+@pytest.fixture
+def game():
+    return Game(room_data, object_data)
+
+
+@pytest.fixture
+def game_in_progress(game):
+    game.process_command('north')
+    game.process_command('open', 'plechovka')
+    game.process_command('take', 'sponky')
+    game.process_command('south')
+    game.process_command('open', 'dvere')
+    return game
--- tests/test_game_logic.py
+++ tests/test_game_logic.py
@@ -2,23 +2,7 @@
 
 import pytest
 
-from engine import Game, InvalidCommand
-from game.data import room_data, object_data
-
-
-@pytest.fixture
-def game():
-    return Game(room_data, object_data)
-
-
-@pytest.fixture
-def game_in_progress(game):
-    game.process_command('north')
-    game.process_command('open', 'plechovka')
-    game.process_command('take', 'sponky')
-    game.process_command('south')
-    game.process_command('open', 'dvere')
-    return game
+from engine import InvalidCommand
 
 
 def test_game_walk_through(game):
