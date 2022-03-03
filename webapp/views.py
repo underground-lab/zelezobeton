313847c4dd268d52b7a51abafe250e617f7b7157
Michal Porteš <michalportes1@gmail.com>
2022-03-03 09:56:44 +0100

    Store game state in session

--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 
+from engine import Game
+from game.data import room_data, object_data
+
 
 def home(request):
-    return render(request, 'home.html', {'message': 'test'})
+    session = request.session
+
+    # retrieve stored game state or create new
+    try:
+        game = Game.from_json(session['game'])
+    except KeyError:
+        game = Game(room_data, object_data)
+
+    # modify game state
+    ...
+
+    # store game state
+    session['game'] = game.to_json()
+
+    return render(request, 'home.html', {'game': game})
