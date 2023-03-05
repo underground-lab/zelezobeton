7f1949bdd9e686a8e9ad0d2479400e0779f74d5e
Michal Porteš <michalportes1@gmail.com>
2023-03-05 16:37:21 +0100

    Remove VERSION file

--- bump_commit_tag.py
+++ bump_commit_tag.py
@@ -13,11 +13,7 @@ except IndexError:
 # bump with poetry
 subprocess.run(f'poetry version {rule}'.split())
 
-# write new version to VERSION
 version = toml.load('pyproject.toml')['tool']['poetry']['version']
-print('Updating VERSION')
-with open('VERSION', 'w', encoding='utf-8') as f:
-    print(version, file=f)
 
 # commit & tag
 subprocess.run(['git', 'commit', '-a', '-m', f'Bump version to {version}'])
