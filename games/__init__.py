869125c618d1b2b8528aec4cd34ad37b2ee45af3
Michal Porteš <michalportes1@gmail.com>
2023-03-10 13:13:51 +0100

    Import game dynamically

---
+++ games/__init__.py
@@ -0,0 +1,21 @@
+from importlib import import_module
+from pathlib import Path
+
+
+def _import_games():
+    for py_file in Path(__file__).parent.glob('*.py'):
+        short_name = py_file.stem
+        if short_name == '__init__':
+            continue
+        module = import_module(f'.{short_name}', __name__)
+        if all(
+            hasattr(module, attr_name)
+            for attr_name in (
+                'title', 'exit_labels', 'action_labels', 'room_data', 'object_data'
+            )
+        ):
+            yield short_name, module
+
+
+_all_games = dict(_import_games())
+game_data = _all_games['default']
