14cf08bc382ef37fa7e25a0f5270dbeb5d09bc06
Michal Porteš <michalportes1@gmail.com>
2023-06-28 10:26:51 +0200

    Simplify key generation

--- generate_key.py
+++ generate_key.py
@@ -1,7 +1,6 @@
+import secrets
 from pathlib import Path
-from random import choices
-from string import ascii_letters
 
-key_path = (Path(__file__).parent / '.key')
-key_path.write_text(''.join(choices(ascii_letters, k=100)), encoding='utf8')
+key_path = Path(__file__).parent / '.key'
+key_path.write_text(secrets.token_hex(), encoding='utf8')
 print('Key written to', key_path)
